ca8c1ee596551e5a811be141ebe4f9dec8567f0b
Tirth Prajapati <tirthprajapati26@gmail.com>
2023-07-12 02:02:20 +0530

    KthMin added

--- Lab01/RandQS.java
+++ Lab01/RandQS.java
@@ -3,19 +3,19 @@ import java.util.*;
 class RandQS{
     public static void main(String args[]){
         QuickSort obj = new QuickSort();
-        int test_count = 10;
+        int test_count = 1;
         
         //[]
         for(int i=1;i<=test_count;i++){
-            // int arr[] = {4, 3, 6, 8, 5, 9, 20, 50, 12, 30, 432, 12, 69, 29, 40, 50, 10, 4, 23, 34, 45, 45, 24,
-            //          89, 799, 45, 80, 50, 10, 30, 43,54,65,76,87, 98, 21, 32, 43, 78, 98, 57, 29, 
-            //          91, 34,54, 64, 19, 82, 87, 65};
+            int arr[] = {4, 3, 6, 8, 5, 9, 20, 50, 12, 30, 432, 12, 69, 29, 40, 50, 10, 4, 23, 34, 45, 45, 24,
+                     89, 799, 45, 80, 50, 10, 30, 43,54,65,76,87, 98, 21, 32, 43, 78, 98, 57, 29, 
+                     91, 34,54, 64, 19, 82, 87, 65};
                     //   int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9,12,13,14,15,16,18}; //sorted
         //  int arr[] = {81,61,51,41,31,21,9 ,8 ,7 ,6 ,5 ,4 ,3 ,2 ,1 ,0}; //reverse
-            int arr[] = {1,61,51,4,231,21,90 ,84 ,72 ,64 ,15 ,40 ,33 ,112 ,21 ,10}; //Random
+            // int arr[] = {1,61,51,4,231,21,90 ,84 ,72 ,64 ,15 ,40 ,33 ,112 ,21 ,10}; //Random
             obj.quicksort(arr,0,arr.length-1);
-            // obj.printArr(arr);
-            System.out.println(i+"th Iteration : "+"Comps = "+obj.count);
+            obj.printArr(arr);
+            // System.out.println(i+"th Iteration : "+"Comps = "+obj.count);
             obj.count = 0;
         }
     }
--- Lab02/Rand_Primality_Test.java
+++ Lab02/Rand_Primality_Test.java
@@ -1,15 +1,36 @@
 import java.util.*;
 class Rand_Primality_Test{
     public static void main(String[] args) {
-        int test_count = 1000000;
+        int test_count = 1;
+        int f_liars = 0;
+        long n =1000000000;
         Rand_Primality_Test_Helper obj = new Rand_Primality_Test_Helper();
-        for(int i = 1 ;i<test_count+1;i++){
-            //  System.out.println(i+"th iteration = "+obj.isPrime(2614150 , 1));
-            if(obj.isPrime(16 , 100) == true){
-                System.out.println("Error at "+i+"th iteration");
-            }
+        // for(int i = 1 ;i<test_count+1;i++){
+        //     // //  System.out.println(i+"th iteration = "+obj.isPrime(2614150 , 1));
+        //     // if(obj.isPrime( , 100) == true){
+        //     //     System.out.println("Error at "+i+"th iteration");
+        //     if(obj.isPrime(4, 100)){
+        //         System.out.println("given number is Prime");
+        //     }else{
+        //         System.out.println("given number is composite");
+        //     }
+        //     // System.out.println(obj.isPrime(1000000008 , 1));
+        // }
+        
+        long exp = n/6000000;
+        System.out.println("expected waiting time : "+exp+" Seconds");
+        for(long i=2;i<n-2;i++){
+            // System.out.println(i+"th iteration");
+            if(obj.mod_exp(i,n-1,n) == 1){
+                System.out.println(i+" is F-Liar");
+                f_liars++;
+            }   
         }
-       
+
+        float  prob = ((float )(f_liars)/(n-3));
+        System.out.println(f_liars);
+        System.out.println(n-3);
+        System.out.println("Probablity : "+((float )(f_liars)/(n-3)));
     }
 }
 class Rand_Primality_Test_Helper{
@@ -35,6 +56,11 @@ class Rand_Primality_Test_Helper{
         return x;
     }
     boolean isPrime(long n,long k){
+        if( n == 1 ){
+            return false;
+        }else if(n==2 || n==3){
+            return true;
+        }
         Random rand = new Random();
         long i=0;
         while(i<k){
